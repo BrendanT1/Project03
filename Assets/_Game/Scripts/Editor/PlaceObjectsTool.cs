869811790089498a79096dc1c6680e550086f2e5
BrendanT <Brendant915@gmail.com>
2024-05-06 00:11:02 -0500

    Building Script & Errors
    Writing script and ran into issues with !HasPlaceableObject and GetCurrentMousePositionInScene() commands not working properly

--- Assets/_Game/Scripts/Editor/PlaceObjectsTool.cs
+++ Assets/_Game/Scripts/Editor/PlaceObjectsTool.cs
@@ -4,17 +4,49 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditor.EditorTools;
 
-public class PlaceObjectsTool : MonoBehaviour
+[EditorTool("Place Objects Tool")]
+public class PlaceObjectsTool : EditorTool
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Texture2D _toolIcon;
+    GUIContent _iconContent;
+
+    private void OnEnable()
     {
-        
+        _iconContent = new GUIContent()
+        {
+            image = _toolIcon,
+            text = "Place Objects Tool",
+            tooltip = "Place Objects Tool"
+        };
     }
 
-    // Update is called once per frame
-    void Update()
+    public override GUIContent toolbarIcon
     {
-        
+        get { return _iconContent; }
+    }
+
+    public override void OnToolGUI(EditorWindow window)
+    {
+        //return when not in scene view
+        if (!(window is SceneView))
+            return;
+
+        //return if we're not active tool
+        if (!ToolManager.IsActiveTool(this))
+            return;
+
+        //return if there's no placeable object in scene
+        if (!HasPlaceableObject)
+            return;
+
+        //draw positional handle
+        Handles.DrawWireDisc(GetCurrentMousePositionInScene(), Vector3.up, 0.5f);
+    }
+    Vector3 GetCurrentMousePositionInScene()
+    {
+        Vector3 mousePosition = Event.current.mousePosition;
+        var placeObject = HandleUtility.PlaceObject(mousePosition, out var newPosition, out var normal);
+        return placeObject ? newPosition :
+            HandleUtility.GUIPointToWorldRay(mousePosition).GetPoint(10);
     }
 }
